eeb7a9a7b860ec4738f095273e580437fbbfd9ea
Chris Stone <chris.stone603@gmail.com>
2024-03-01 21:38:15 -0500

    feat: introduces sidebar sheet to add widgets, counter and gloomhaven

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -5,19 +5,10 @@ import { useState } from "react";
 import WidgetsCard from "~/components/WidgetsCard";
 
 export default function Game({ params }: { params: { id: string } }) {
-  const [counters, setCounters] = useState([
-    { name: "HP" },
-    { name: "XP" },
-    { name: "Gold" },
-  ]);
-  const [conditionSelectors, setConditionSelectors] = useState(["gloomhaven"]);
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] p-10 text-white">
       <h1>Game {params.id}</h1>
-      <WidgetsCard
-        counters={counters}
-        conditionSelectors={conditionSelectors}
-      />
+      <WidgetsCard />
     </main>
   );
 }
--- src/components/WidgetsCard.tsx
+++ src/components/WidgetsCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -5,19 +8,60 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "~/components/ui/sheet";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
 import { Button } from "./ui/button";
 import Plus from "./icons/Plus";
 
 import Counter from "./Counter";
 import ConditionSelector from "./ConditionSelector";
+import XMark from "./icons/XMark";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
-export default function WidgetsCard({
-  counters,
-  conditionSelectors,
-}: {
-  counters: { name: string }[];
-  conditionSelectors: string[];
-}) {
+export default function WidgetsCard() {
+  const [counters, setCounters] = useState<{ name: string }[]>([
+    { name: "HP" },
+    { name: "XP" },
+    { name: "Gold" },
+  ]);
+  const [conditionSelectors, setConditionSelectors] = useState<string[]>([]);
+  const [newCounterName, setNewCounterName] = useState<string>("");
+  const [newConditionSelector, setNewConditionSelector] = useState<string>("");
+
+  function addCounter(name: string) {
+    setCounters((current) => [...current, { name }]);
+  }
+
+  function removeCounter(name: string) {
+    setCounters((current) =>
+      current.filter((counter) => counter.name !== name),
+    );
+  }
+
+  function addConditionSelector(name: string) {
+    setConditionSelectors((current) => [...current, name]);
+  }
+
+  function removeConditionSelector(name: string) {
+    setConditionSelectors((current) =>
+      current.filter((selector) => selector !== name),
+    );
+  }
+
   return (
     <Card className="w-full">
       <CardHeader>
@@ -25,16 +69,81 @@ export default function WidgetsCard({
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
         {counters.map((counter) => (
-          <Counter key={counter.name} name={counter.name} />
+          <div key={counter.name} className="flex justify-between gap-1">
+            <Counter name={counter.name} />
+            <Button
+              variant={"destructive"}
+              size={"icon"}
+              onClick={() => removeCounter(counter.name)}
+            >
+              <XMark />
+            </Button>
+          </div>
         ))}
-        {conditionSelectors.map((selector) => (
-          <ConditionSelector key={selector} options={selector} />
+        {conditionSelectors.map((selector, i) => (
+          <ConditionSelector key={`selector-${i}`} options={selector} />
         ))}
       </CardContent>
       <CardFooter className="flex justify-end">
-        <Button>
-          <Plus />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>
+              <Plus />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Add widgets</SheetTitle>
+            </SheetHeader>
+            <ul className="flex flex-col gap-4">
+              <li className="flex w-full max-w-sm items-end space-x-2">
+                <div className="grid w-full max-w-sm items-center gap-1.5 text-slate-950">
+                  <Label htmlFor="counter-name">Counter</Label>
+                  <Input
+                    id="counter-name"
+                    placeholder="Gold"
+                    onChange={(e) => setNewCounterName(e.target.value)}
+                  />
+                </div>
+                <Button
+                  onClick={() => addCounter(newCounterName)}
+                  disabled={!newCounterName}
+                >
+                  Create
+                </Button>
+              </li>
+              <li className="flex w-full max-w-sm items-end space-x-2">
+                <div className="grid w-full max-w-sm items-center gap-1.5 text-slate-950">
+                  <Label htmlFor="condition-selector">Condition Selector</Label>
+                  <Select
+                    onValueChange={(value: string) => {
+                      console.log(value);
+                      setNewConditionSelector(value);
+                    }}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Condition Presets" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="gloomhaven">Gloomhaven</SelectItem>
+                      <SelectItem value="dnd">DnD</SelectItem>
+                      <SelectItem value="custom">Custom</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <Button
+                  onClick={() => {
+                    console.log(newConditionSelector);
+                    addConditionSelector(newConditionSelector);
+                  }}
+                  disabled={!newConditionSelector}
+                >
+                  Create
+                </Button>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </CardFooter>
     </Card>
   );
